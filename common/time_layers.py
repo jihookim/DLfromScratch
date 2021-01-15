af1e317ec21d775665498d5403db76338a5f602a
jihookim <yomalub@naver.com>
2021-01-15 15:12:18 +0900

    First Commit

--- common/time_layers.py
+++ common/time_layers.py
@@ -296,5 +296,154 @@ class TimeLSTM:
         self.layers=[]
         hs=np.empty((N,T,H),dtype='f')
 
+        if not self.stateful or self.h is None:
+            self.h=np.zeros((N,H),dtype='f')
+        if not self.stateful or self.c is None:
+            self.c=np.zeros((N,H),dtype='f')
+
+        for t in range(T):
+            layer=LSTM(*self.params)
+            self.h,self.c=layer.forward(xs[:,t,:],self.h,self.c)
+            hs[:,t,:]=self.h
+
+            self.layers.append(layer)
+
+        return hs
+
+    def backward(self,dhs):
+        Wx,Wh,b=self.params
+        N,T,H=dhs.shape
+        D=Wx.shape[0]
+
+        dxs=np.empty((N,T,D),dtype='f')
+        dh,dc=0,0
+
+        grads=[0,0,0]
+        for t in reversed(range(T)):
+            layer=self.layers[t]
+            dx,dh,dc=layer.backward(dhs[:,t,:]+dh,dc)
+            dxs[:,t,:]=dx
+            for i,grad in enumerate(layer.grads):
+                grads[i]+=grad
+
+        for i,grad in enumerate(grads):
+            self.grads[1][...]=grad
+
+        self.dh=dh
+        return dxs
+
+    def set_state(self,h,c=None):
+        self.h,self.c=h,c
+
+    def reset_state(self):
+        self.h,self.c=None,None
 
 
+#단어 ID를 단어의 분산 표현(단어 벡터)로 변환
+class TimeEmbedding:
+    def __init(self,W):
+        self.params=[W]
+        self.grads=[np.zeros_like(W)]
+        self.layers=None
+        self.W=W
+
+    def forward(self,xs):
+        N,T=xs.shape
+        V,D=self.W.shape
+
+        out=np.empty((N,T,D),dtype='f')
+        self.layers=[]
+
+        for t in range(T):
+            layer=Embedding(self.W)
+            out[:,t,:]=layer.forward(xs[:,t])
+            self.layers.append(layer)
+
+        return out
+
+    def backward(self,dout):
+        N,T,D=dout.shape
+
+        grad=0
+        for t in range(T):
+            layer=self.layers[t]
+            layer.backward(dout[:,t,:])
+            grad+=layer.grads[0]
+
+        self.grads[0][...]=grad
+        return None
+
+
+class TimeAffine:
+    def __init__(self,W,b):
+        self.params=[W,b]
+        self.grads=[np.zeros_like(W),np.zeros_like(b)]
+        self.x=None
+
+    def forward(self,x):
+        N,T,D=x.shape
+        W,b=self.params
+
+        rx=x.reshape(N*T,-1)
+        out=np.dot(rx,W)+b
+        self.x=x
+        return out.reshape(N,T,-1)
+
+    def backward(self,dout):
+        x=self.x
+        N,T,D=x.shape
+        W,b=self.params
+
+        dout=dout.reshape(N*T,-1)
+        rx=x.reshape(N*T,-1)
+
+        db=np.sum(dout,axis=0)
+        dW=np.dot(rx.T,dout)
+        dx=np.dot(dout,W.T)
+        dx=dx.reshape(*x.shape)
+
+        self.grads[0][...]=dW
+        self.grads[1][...]=db
+
+        return dx
+
+class TimeSoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.cache = None
+        self.ignore_label = -1
+
+    def forward(self, xs, ts):
+        N, T, V = xs.shape
+
+        if ts.ndim == 3:  # 教師ラベルがone-hotベクトルの場合
+            ts = ts.argmax(axis=2)
+
+        mask = (ts != self.ignore_label)
+
+        # バッチ分と時系列分をまとめる（reshape）
+        xs = xs.reshape(N * T, V)
+        ts = ts.reshape(N * T)
+        mask = mask.reshape(N * T)
+
+        ys = softmax(xs)
+        ls = np.log(ys[np.arange(N * T), ts])
+        ls *= mask  # ignore_labelに該当するデータは損失を0にする
+        loss = -np.sum(ls)
+        loss /= mask.sum()
+
+        self.cache = (ts, ys, mask, (N, T, V))
+        return loss
+
+    def backward(self, dout=1):
+        ts, ys, mask, (N, T, V) = self.cache
+
+        dx = ys
+        dx[np.arange(N * T), ts] -= 1
+        dx *= dout
+        dx /= mask.sum()
+        dx *= mask[:, np.newaxis]  # ignore_labelに該当するデータは勾配を0にする
+
+        dx = dx.reshape((N, T, V))
+
+        return dx
